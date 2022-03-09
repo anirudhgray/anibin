f6370cc7f27482dce27bf33f8b4ba8d936a20c61
anirudh <anirudh04mishra@gmail.com>
2022-03-09 13:00:51 +0530

    Fix allowed origins.

--- backstage-machinery/src/index.js
+++ backstage-machinery/src/index.js
@@ -9,10 +9,15 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', [
-    'http://localhost:3000',
+  const allowedOrigins = [
     'https://anibin.netlify.app',
-  ]);
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
